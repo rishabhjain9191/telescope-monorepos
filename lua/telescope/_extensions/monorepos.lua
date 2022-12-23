380724c5ed1f0bd03469207ea6ef7c152c08c117
Rishabh Kumar Jain <rishjain@adobe.com>
2022-12-23 22:02:26 +0530

    renamed files

--- lua/telescope/_extensions/monorepos.lua
+++ lua/telescope/_extensions/monorepos.lua
@@ -1,10 +1,10 @@
-local TelescopeMonorepos = require('telescope_monorepos');
+local TelescopeMonorepos = require('monorepos');
 
 return require("telescope").register_extension {
   --[[ setup = function(ext_config, config) ]]
   --[[   -- access extension config and user config ]]
   --[[ end, ]]
   exports = {
-    telescope_monorepos = TelescopeMonorepos.show_monorepos
+    monorepos = TelescopeMonorepos.show_monorepos
   },
 }
